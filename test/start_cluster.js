72e7336fa395a5cbdc568a2da9c5dfeccb6900c3
Scott Nonnenberg <scott@nonnenberg.com>
2014-04-28 13:54:42 -0700

    Adding some code to test top-level error handling

--- test/start_cluster.js
+++ test/start_cluster.js
@@ -6,7 +6,19 @@ process.env = require('../env.json');
 var cluster = require('../src/server/index');
 
 cluster({
+  master: function() {
+    var master = new cluster.Master();
+    master.start();
+
+    // setTimeout(function() {
+    //   throw new Error('top-level master crash!');
+    // }, 1000);
+  },
   worker: function() {
     require('./start_server');
+
+    // setTimeout(function() {
+    //   throw new Error('top-level worker crash!');
+    // }, 5100);
   }
 });
