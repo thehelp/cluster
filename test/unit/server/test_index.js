1ba848ab8277f858f6f3afaba85b8777bbcd68fd
Scott Nonnenberg <scott@nonnenberg.com>
2014-10-07 21:21:14 -0700

    Unit tests for the public top-level API

--- test/unit/server/test_index.js
+++ test/unit/server/test_index.js
@@ -2,12 +2,22 @@
 'use strict';
 
 var test = require('thehelp-test');
+var expect = test.expect;
 var index = require('../../../src/server/index');
 
 describe('thehelp-cluster', function() {
 
-  it('works!', function() {
-    test.expect(index).to.exist;
+  it('returns a function as the top-level object', function() {
+    expect(index).to.be.a('function');
+  });
+
+  it('exposes all the right sub-keys', function() {
+    expect(Object.keys(index)).to.have.length(4);
+
+    expect(index).to.have.property('Master').that.is.a('function');
+    expect(index).to.have.property('Graceful').that.is.a('function');
+    expect(index).to.have.property('Startup').that.is.a('function');
+    expect(index).to.have.property('DomainMiddleware').that.is.a('function');
   });
 
 });
