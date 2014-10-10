74b5db948dc7348d924966654b7931a1b3cd81ca
Scott Nonnenberg <scott@nonnenberg.com>
2014-10-09 18:25:48 -0700

    Add unit test for GracefulExpress._once to be sure

--- test/unit/server/test_graceful_express.js
+++ test/unit/server/test_graceful_express.js
@@ -51,5 +51,21 @@ describe('GracefulExpress', function() {
     });
   });
 
+  describe('#_once', function() {
+    it('only calls once and passes all arguments through', function() {
+      var fn = sinon.spy(function(left, right) {
+        expect(left).to.equal('yes');
+        expect(right).to.equal('no');
+      });
+
+      var once = graceful._once(fn);
+      once('yes', 'no');
+      once('yes', 'no');
+      once('yes', 'no');
+
+      expect(fn).to.have.property('callCount', 1);
+    });
+  });
+
 });
 
