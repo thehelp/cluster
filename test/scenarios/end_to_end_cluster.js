95e8614fbb6ddd303ce28dc4311b7f82ff63a15b
Scott Nonnenberg <scott@nonnenberg.com>
2014-10-11 16:07:52 -0700

    Add override of winston logging to end-to-end test

--- test/scenarios/end_to_end_cluster.js
+++ test/scenarios/end_to_end_cluster.js
@@ -5,20 +5,37 @@ var path = require('path');
 var core = require('thehelp-core');
 core.env.merge(path.join(__dirname, '../../env.json'));
 
+var winston = require('winston');
+var log = {
+  info: function(text) {
+    winston.info('PREFIX ' + text);
+  },
+  warn: function(text) {
+    winston.warn('PREFIX ' + text);
+  },
+  error: function(text) {
+    winston.error('PREFIX ' + text);
+  }
+};
+
 var cluster = require('../../src/server/index');
 var ld = require('thehelp-last-ditch');
 
 var lastDitch = new ld.LastDitch({
-  targets: ['stderr']
+  targets: ['stderr'],
+  log: log
 });
 
 cluster.Graceful.start({
-  messenger: lastDitch.go
+  messenger: lastDitch.go,
+  log: log
 });
 
 cluster({
+  log: log,
   masterOptions: {
-    spinTimeout: 100
+    spinTimeout: 100,
+    log: log
   },
   worker: function() {
     require('./end_to_end_server');
