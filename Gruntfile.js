04832ee29776d106e2822841957e793e9472f580
Scott Nonnenberg <scott@nonnenberg.com>
2014-10-14 22:30:52 -0700

    Remove useless doc comments from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,6 @@
 
 var GruntConfig = require('thehelp-project').GruntConfig;
 
-// We simply create an instance of the `GruntConfig` class from
-// `thehelp-project`, then call the register functions we need.
 module.exports = function(grunt) {
   var config = new GruntConfig(grunt);
 
